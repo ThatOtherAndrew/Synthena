32ca9a25f4535a52d84df9c1102463e733c5c4f9
Andromeda <stroev.andrew@gmail.com>
2025-11-15 09:18:12 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Synthena";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "synthena";
+        packages = with pkgs; [
+          bun
+        ];
+      };
+    };
+}
