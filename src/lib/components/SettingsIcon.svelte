0457766cea96b5132a2c4193a6eeb435da14f25a
Andromeda <stroev.andrew@gmail.com>
2025-11-15 23:00:27 -0500

    Make settings icon prettier

--- src/lib/components/SettingsIcon.svelte
+++ src/lib/components/SettingsIcon.svelte
@@ -1,15 +1,20 @@
 <script>
-import gearIcon from '$lib/assets/gear.png';
+	import gearIcon from '$lib/assets/gear.png';
 </script>
 
-<img src={gearIcon} />
+<img src={gearIcon} alt="Settings" />
 
 <style>
-    img {
-        position: absolute;
-        top: 1rem;
-        right: 1rem;
-        /* font-size: 1rem; */
-        width: 2.5rem !important;
-    }
+	img {
+		position: absolute;
+		top: 1rem;
+		right: 1rem;
+		width: 2.5rem;
+		transition: transform 0.2s ease;
+	}
+
+	img:hover {
+		cursor: pointer;
+		transform: rotate(60deg);
+	}
 </style>
